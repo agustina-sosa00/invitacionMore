2554fdd779b04aa221c27e344537911c7ef7af35
Agustina <agustinaayelensosa@gmail.com>
2024-07-23 17:05:22 -0300

    agregue home a app

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Carrousel } from "./view/Carrousel/Carrousel";
+import { Home } from "./view/Home/Home";
 import { Info } from "./view/Info/Info";
 
 function App() {
 
   return (
     <>
+      <Home/>
       <Carrousel />
       <Info />
     </>
